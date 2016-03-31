d5440072c7ad680363d165310917eb5c9809be2d
Felipe Lima <felipepassine@gmail.com>
2016-03-31 20:12:00 -0300

    created authentication method

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
+    before { controller.stub(:authenticate_user!).and_return true }
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
